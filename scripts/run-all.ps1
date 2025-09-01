7a7d890fb0653daca8531317213070c81357d87c
Charitha-2003 <139891780+Charitha-2003@users.noreply.github.com>
2025-09-01 12:49:00 +0530

    Delete scripts directory

--- scripts/run-all.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Starting Eureka -> Customer -> Admin -> Gateway ..."
-Start-Process powershell -ArgumentList "-NoExit","-Command","cd .\eureka-server; mvn -q -DskipTests spring-boot:run"
-Start-Sleep -Seconds 6
-Start-Process powershell -ArgumentList "-NoExit","-Command","cd .\customer-service; mvn -q -DskipTests spring-boot:run"
-Start-Sleep -Seconds 3
-Start-Process powershell -ArgumentList "-NoExit","-Command","cd .\admin-service; mvn -q -DskipTests spring-boot:run"
-Start-Sleep -Seconds 3
-Start-Process powershell -ArgumentList "-NoExit","-Command","cd .\api-gateway; mvn -q -DskipTests spring-boot:run"
